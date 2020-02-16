3cc6608e3666f328f081148f6b2591b9bb695917
Armando Flores <aflores57@gmail.com>
2020-02-15 21:34:51 -0500

    step01 - Styling, State and Partial App Component

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language })
+  }
+
+  render() {
+    return <div className="ui container">
+      <div>
+        Select language:
+        <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+        <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+      </div>
+      {this.state.language}
+    </div>
+  }
+}
+
+export default App
