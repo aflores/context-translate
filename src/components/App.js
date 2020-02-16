6bed4709f31151aa4f9ef5421619863d43ad1415
Armando Flores <aflores57@gmail.com>
2020-02-16 16:52:03 -0500

    Context, Provider, Consumer, contextType

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import UserCreate from './UserCreate'
+import LanguageContext from '../contexts/LanguageContext'
 
 class App extends React.Component {
-  state = { language: 'english' };
+  state = { language: 'dutch' };
 
   onLanguageChange = language => {
     this.setState({ language })
@@ -15,7 +16,9 @@ class App extends React.Component {
         <i className="flag us" onClick={() => this.onLanguageChange('english')} />
         <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
       </div>
-      <UserCreate />
+      <LanguageContext.Provider value={this.state.language}>
+        <UserCreate />
+      </LanguageContext.Provider>
     </div>
   }
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen'; 
+  }
+
   render() {
     return(
-      <button className="ui button primary">Submint</button>
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
     );
   }
 }
